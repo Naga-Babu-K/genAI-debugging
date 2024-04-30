bb21abfd140a5169384139ef64a5a93542863476
Naga-Babu-K <142527703+Naga-Babu-K@users.noreply.github.com>
2024-04-30 10:15:57 +0530

    Create rag.py

---
+++ Rag /rag.py
@@ -0,0 +1,60 @@
+
+import google.generativeai as gemini
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.messages import HumanMessage,SystemMessage
+from langchain_core.prompts import ChatPromptTemplate,HumanMessagePromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough 
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import streamlit as st 
+import nltk
+nltk.download('punkt')
+
+st.title("Retrieval Augmented Generation System for Q & A Chat Bot")
+st.markdown("may be i can clarify you'r doubt, why don't you try me?")
+
+
+f = open(r"C:\Users\Erik\Desktop\Ai elite 13\Gemimi_ai\New Text Document.txt")
+api_key = f.read()
+
+
+chat_model = ChatGoogleGenerativeAI(google_api_key=api_key, model="gemini-1.5-pro-latest")
+
+embedding_model = GoogleGenerativeAIEmbeddings(google_api_key=api_key, model="models/embedding-001")
+
+connection = Chroma(persist_directory="data/Chroma_db_for_rag", embedding_function=embedding_model)
+
+
+retriever = connection.as_retriever(search_kwargs={"k": 5})
+
+# query
+user_query = st.text_input(":blue[Please Enter your query...]")
+
+# Chatbot prompt templates
+chat_template = ChatPromptTemplate.from_messages([
+    SystemMessage(content="You are a Helpful AI Bot. You take the context and question from user. Your answer should be based on the specific context."),
+    HumanMessagePromptTemplate.from_template("Answer the question based on the given context.\nContext: {Context}\nQuestion: {question}\nAnswer:")
+])
+
+# Output parser for chatbot response
+output_parser = StrOutputParser()
+
+# Function to format retrieved documents
+def format_docs(docs):
+    formatted_content = "\n\n".join(doc.page_content.strip() for doc in docs if doc.page_content.strip())
+    return formatted_content if formatted_content else "No relevant context found."
+
+# RAG chain for chatbot interaction
+rag_chain = (
+    {"Context": retriever | format_docs, "question": RunnablePassthrough()}
+    | chat_template
+    | chat_model
+    | output_parser
+)
+
+if st.button("Result"):
+    if user_query:
+        response = rag_chain.invoke(user_query)
+        st.write(response)
